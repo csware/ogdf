6d91a740b514e6de3bb43504b93ca1de540ba47c
Sven Strickroth <email@cs-ware.de>
2025-11-11 17:45:06 +0100

    OGDF specific adaptions to pugixml
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- include/ogdf/lib/pugixml/pugiconfig.h
+++ include/ogdf/lib/pugixml/pugiconfig.h
@@ -14,6 +14,10 @@
 #ifndef HEADER_PUGICONFIG_HPP
 #define HEADER_PUGICONFIG_HPP
 
+#include <ogdf/basic/internal/config.h>
+
+#define PUGIXML_API OGDF_EXPORT
+
 // Uncomment this to enable wchar_t mode
 // #define PUGIXML_WCHAR_MODE
 
@@ -27,13 +31,14 @@
 // #define PUGIXML_NO_STL
 
 // Uncomment this to disable exceptions
-// #define PUGIXML_NO_EXCEPTIONS
+#define PUGIXML_NO_EXCEPTIONS
 
 // Set this to control attributes for public classes/functions, i.e.:
 // #define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
 // #define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
 // #define PUGIXML_FUNCTION __fastcall // to set calling conventions to all public functions to fastcall
 // In absence of PUGIXML_CLASS/PUGIXML_FUNCTION definitions PUGIXML_API is used instead
+#define PUGIXML_API OGDF_EXPORT
 
 // Tune these constants to adjust memory-related behavior
 // #define PUGIXML_MEMORY_PAGE_SIZE 32768
